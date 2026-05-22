1afa1a48a640742fc69d6ae47360743c983cd347
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-05-22 22:34:33 +0500

    the card page is updated and navbar is now clean

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://ielts-cefr-predictions-speaking.vercel.app/api/v1",
 });
 
 // Request interceptor
