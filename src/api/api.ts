0d06ebbde177d6ad97a5e5983bd2104929131462
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-05-31 00:53:00 +0500

    base url added

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = process.env.BASE_URL;
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api/v1", // ✅ To'g'ri URL
+  baseURL: BASE_URL, // ✅ To'g'ri URL
 });
 
 // Request interceptor
